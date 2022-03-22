28c2ea018c305628b5c7ce740b593094a150ab7c
wagnerbintb <wagner@bintb.com>
2022-03-22 17:21:46 -0300

    gorm repository

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
-	gorm.io/gorm v1.23.2 // indirect
+	gorm.io/gorm v1.23.3 // indirect
 )
---
+++ repositories/shortenedUrls/GormShortenedUrlsRepository.go
@@ -0,0 +1,59 @@
+package shortenedUrls
+
+import (
+	"errors"
+	"url-shortener/entities"
+
+	"gorm.io/gorm"
+)
+
+type repoGorm struct {
+	writer *gorm.DB
+	reader *gorm.DB
+}
+
+func NewGormRepository(writer, reader *gorm.DB) IShortnedUrlsRepository {
+	return &repoGorm{writer: writer, reader: reader}
+}
+
+func (repo *repoGorm) Create(shortenedUrl entities.ShortenedUrl) error {
+
+	repo.writer.Table("shortened_urls").Create(&shortenedUrl)
+
+	return repo.writer.Error
+}
+
+func (repo *repoGorm) FindAll() ([]entities.ShortenedUrl, error) {
+	var shortenedUrls []entities.ShortenedUrl
+	repo.reader.Table("shortened_urls").Find(&shortenedUrls)
+
+	if repo.reader.Error != nil {
+		return nil, errors.New("shortened urls not found")
+	}
+
+	return shortenedUrls, nil
+}
+
+func (repo *repoGorm) FindByShortenedID(id string) (entities.ShortenedUrl, error) {
+	var shortenedUrlById entities.ShortenedUrl
+
+	repo.reader.Table("shortened_urls").Find(&shortenedUrlById)
+
+	if repo.reader.Error != nil {
+		return entities.ShortenedUrl{}, errors.New("shortened url not found")
+	}
+
+	return shortenedUrlById, nil
+}
+
+func (repo *repoGorm) FindByUrl(url string) (entities.ShortenedUrl, error) {
+	var shortenedUrlByUrl entities.ShortenedUrl
+
+	repo.reader.Table("shortened_urls").Where("url = ?", url).Find(&shortenedUrlByUrl)
+
+	if repo.reader.Error != nil {
+		return entities.ShortenedUrl{}, errors.New("shortened url not found")
+	}
+
+	return shortenedUrlByUrl, nil
+}
